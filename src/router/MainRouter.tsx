c62885d5df207c08d407820d4ec2728f13cd7d2b
kimulchan <leerabi577@gmail.com>
2021-11-11 23:15:55 +0900

    📑:: main Router 추가

---
+++ src/router/MainRouter.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router";
+
+
+const MainRouter = ()=>{
+  return (
+    <Routes>
+      <Route path="/" ></Route>
+    </Routes>
+  )
+}
+
+export default MainRouter;
