63b3e5d4f0e25aa531469872ac5492c5eccc0fad
kimulchan <leerabi577@gmail.com>
2021-11-11 23:14:51 +0900

    📑 :: RootRouter 추가

---
+++ src/router/RootRouter.tsx
@@ -0,0 +1,22 @@
+import React,{ Suspense } from "react";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+const MainRouter = React.lazy(()=>import('./MainRouter'));
+
+const RootRouter = () => {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <Suspense fallback={<div>...loading</div>}>
+              <MainRouter></MainRouter>
+            </Suspense>
+          }
+        ></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default RootRouter;
