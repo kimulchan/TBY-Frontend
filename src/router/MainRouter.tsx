5c95a1aa9f28591a041c079b834dbca7e1359028
kimulchan <leerabi577@gmail.com>
2021-11-15 09:18:21 +0900

    ⚡ :: MainRouter 추가

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -1,11 +1,23 @@
 import { Route, Routes } from "react-router";
-
+import Footer from "../components/footer";
+import Header from "../components/header";
+import MainPage from "../components/main";
+import SignUpModal from "../components/modal/SignUp";
+import NotFound from "../components/notFound";
+import VideoList from "../components/videoBox";
 
 const MainRouter = ()=>{
   return (
+    <>
+    <Header/>
     <Routes>
-      <Route path="/" ></Route>
+      <Route path="/" element={<MainPage></MainPage>}/>
+      <Route path="/lecture/:category" element={<VideoList></VideoList>}/>
+      <Route path="/lecture/:category/:subject"element={<VideoList></VideoList>}/>
+      <Route path="*" element={<NotFound></NotFound>}></Route>
     </Routes>
+    <Footer/>
+    </>
   )
 }
 
