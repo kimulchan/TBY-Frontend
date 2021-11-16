b2a616dba527d1f1b88cc3cf56fa327ef1aaa1ca
kimulchan <leerabi577@gmail.com>
2021-11-16 09:17:22 +0900

    ⚙ :: provider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import { Provider } from 'react-redux';
+import store from './module/store';
+import RootRouter from './router/RootRouter';
+import GlobalStyle from "./style/globalStyle";
 function App() {
   return (
-   <>
-  
-   </>
+    <Provider store={store}>
+      <RootRouter></RootRouter>
+      <GlobalStyle/>
+    </Provider>
   );
 }
 
