f7f87bc242145510aa438de188ba3830ecdd8ffe
kimulchan <leerabi577@gmail.com>
2021-11-16 23:22:35 +0900

    ⚡ :: modal redux 코드 추가

--- src/module/action/modal.ts
+++ src/module/action/modal.ts
@@ -1,11 +1,12 @@
 import { ActionType, createAction } from "typesafe-actions";
 
 
-export const SIGNUP_SETMODAL = "modal/SIGNUP_SETMODAL" as const;
-
+export const SIGNIN_SETMODAL = "modal/SIGNIN_SETMODAL" as const;
+export const SIGNUP_SETMODAL = "modal/SIGNUP_SETMODAL" as const
+export const setSigninModal = createAction(SIGNIN_SETMODAL)();
 export const setSignupModal = createAction(SIGNUP_SETMODAL)();
 
 
-const actions = {setSignupModal};
+const actions = {setSigninModal,setSignupModal};
 
 export type modalActions = ActionType<typeof actions>
--- src/module/reducer/modal.ts
+++ src/module/reducer/modal.ts
@@ -1,14 +1,16 @@
 import { createReducer } from "typesafe-actions"
 import { modalType } from "../../types";
-import { modalActions, SIGNUP_SETMODAL } from "../action/modal"
+import { modalActions, SIGNIN_SETMODAL, SIGNUP_SETMODAL } from "../action/modal"
 
 
 const initState:modalType= {
+  isSigninOpen:false,
   isSignupOpen:false
 }
 
 
 const modalReducer = createReducer<modalType,modalActions>(initState,{
+  [SIGNIN_SETMODAL]:(state) =>({...state,isSigninOpen:!state.isSigninOpen}),
   [SIGNUP_SETMODAL]:(state) =>({...state,isSignupOpen:!state.isSignupOpen})
 })
 
