ac49df99c7e6da5f273a238a2b49bc8f03d548a5
kimulchan <leerabi577@gmail.com>
2021-11-16 23:20:19 +0900

    ⚡ :: auth modal type 추가

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+import { type } from "os";
 
 export type subjectName = "JavaScript" | "Java" | "Swift" | "Python" | "Go" | "HTML/CSS" | "TypeScript";
 
@@ -12,5 +13,29 @@ export type categoryType = Record<categoryName,subjectType[]>;
 
 //modalType
 export interface modalType {
+  isSigninOpen:boolean
   isSignupOpen:boolean
 }
+
+//login (signin)
+export type authInfoList = "id" | "password" | "passwordCheck";
+export type loginInfo = Record<authInfoList,string>
+export interface authInputActionType {
+  type:authInfoList;
+  value:string;
+}
+
+//auth modal type
+export type modalInputType = {
+  infoName:string,
+  type:authInfoList
+  inputType?:string
+}
+export type authModalConstanceType ={
+  title:string;
+  inputs:modalInputType[];
+  discription:string;
+}
+export type authModalList = "signin" | "signup"; 
+export type authModalType =Record<authModalList,authModalConstanceType>
+
