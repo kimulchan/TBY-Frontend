ca26e3db14395db1f50efa3b98fd85cae89c58ad
kimulchan <leerabi577@gmail.com>
2021-11-17 23:24:28 +0900

    ⚡ :: auth reducer 추가

--- src/module/reducer/auth.ts
+++ src/module/reducer/auth.ts
@@ -1,16 +1,22 @@
 import { createReducer } from "typesafe-actions"
-import { loginInfo } from "../../types"
-import { INPUT, authActions } from "../action/auth"
+import { authInitType } from "../../types"
+import { INPUT, authActions, ON_LOGIN, ON_LOGIN_SUCCESS, ON_LOGIN_FAILURE } from "../action/auth"
 
 
-const initState:loginInfo ={
-  id:"",
-  password:"",
-  passwordCheck:"",
+const initState:authInitType ={
+  loginInfo:{
+    id:"",
+    password:"",
+    passwordCheck:""
+  },
+  lodding:false
 }
 
-const authReducer = createReducer<loginInfo,authActions>(initState,{
-  [INPUT]: (state,paylode)=>({...state,[paylode.payload.type]:paylode.payload.value})
+const authReducer = createReducer<authInitType,authActions>(initState,{
+  [INPUT]: (state,paylode)=>({...state,loginInfo:{...state.loginInfo,[paylode.payload.type]:paylode.payload.value}}),
+  [ON_LOGIN]:(state)=>({...state,lodding:true}),
+  [ON_LOGIN_SUCCESS]:(state)=>({...state,lodding:false}),
+  [ON_LOGIN_FAILURE]:(state)=>({...state,lodding:false})
 })
 
 export default authReducer;
