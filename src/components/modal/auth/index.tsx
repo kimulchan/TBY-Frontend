8fb77ce60a7f6cf236310c0e04acfc5822c520d8
kimulchan <leerabi577@gmail.com>
2021-11-24 21:41:15 +0900

    ⚡ :: auth 모달 구현

--- src/components/modal/auth/index.tsx
+++ src/components/modal/auth/index.tsx
@@ -1,21 +1,22 @@
-
-import styled from "styled-components";
-import{Close} from "@styled-icons/evaicons-solid";
-import useModal from "../../../utils/hooks/modal";
-import { authModal } from "../../../constance";
-import React from "react";
-import { authInfoList, authInputActionType, authModalConstanceType} from "../../../types";
+import styled from 'styled-components';
+import { Close } from '@styled-icons/evaicons-solid';
+import useModal from '../../../utils/hooks/modal';
+import React from 'react';
+import {
+  authInfoList,
+  authInitType,
+  authInputActionType,
+  authModalConstanceType,
+} from '../../../types';
 const Background = styled.div`
   position: fixed;
   top: 0;
   left: 0;
   bottom: 0;
   right: 0;
-  
-  background-color: rgba(0,0,0,0.8);
-  z-index:0;
-` 
-
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 0;
+`;
 const ModalContainer = styled.div`
   position: fixed;
   left: 50%;
@@ -27,36 +28,44 @@ const ModalContainer = styled.div`
   text-align: center;
   font-size: 35px;
   font-weight: 600;
-  border: 2px solid white;
-`
+  background: white;
+  div > {
+    color: #664ceb;
+  }
+`;
 const CloseIcon = styled(Close)`
-    position: absolute;
-    top: 0.5rem;
-    right: 0.5rem;
-    width: 2rem;
-    cursor: pointer;
+  position: absolute;
+  top: 0.5rem;
+  right: 0.5rem;
+  width: 2rem;
+  cursor: pointer;
+  color: #664ceb;
+  filter: brightness(0);
 `;
 const InfoInput = styled.div`
   display: flex;
   flex-direction: column;
-  >p{
+
+  > p {
     font-size: 18px;
     font-weight: 600;
     margin: 20px 0 10px 0;
+    color: black;
   }
 
-  >input{
-    
+  > input {
     height: 40px;
     margin-bottom: 20px;
     border: none;
     border-radius: 20px;
+    background-color: white;
     color: black;
     font-weight: 600;
     font-size: 17px;
     text-align: center;
-}
-`
+    border: 2px solid #664ceb;
+  }
+`;
 const SubmitButton = styled.button`
   width: 150px;
   height: 40px;
@@ -66,53 +75,81 @@ const SubmitButton = styled.button`
   border: none;
   margin-top: 40px;
   cursor: pointer;
-  color: white;
-  background:linear-gradient(to bottom right,blue,pink); 
-`
+  color: black;
+  background-color: white;
+  transition: all 0.5s;
+  border: 3px solid #664ceb;
+  :hover {
+    background: #664ceb;
+    color: white;
+  }
+`;
 const Discription = styled.p`
+  margin: 0;
+  color: black;
   font-size: 14px;
   margin: 10px 0;
   cursor: pointer;
-`
-interface Props extends authModalConstanceType{
-  setInput:(paylode:authInputActionType)=>void;
-  onLogin:()=>void;
+  transition: color 0.5s;
+  :hover {
+    color: #664ceb;
+  }
+`;
+
+interface Props extends authModalConstanceType, authInitType {
+  setInput: (paylode: authInputActionType) => void;
+  onJoin: () => void;
+  onLogin: () => void;
+  resetInput: () => void;
 }
 
-const AuthModals:React.FC<Props> =(props)=>{
-  const setModal = useModal().setState
-  const {inputs,title,discription,setInput,onLogin}=props;
+const AuthModals: React.FC<Props> = props => {
+  const setModal = useModal().setState;
+  const { inputs, title, discription, onJoin, loginInfo, setInput, resetInput, onLogin } = props;
 
-  const onChagneClose = ()=>{
-    title==="JOIN"&& setModal.setSignup()
-    title==="LOGIN"&& setModal.setSignin() 
-  }
-  const onChangeOtherModal = ()=>{
+  
+  const onSubmitClick = () => {
+    title === 'JOIN' && onJoin();
+    title === 'LOGIN' && onLogin();
+  };
+
+  const onChagneClose = () => {
+    resetInput();
+    title === 'JOIN' && setModal.setSignup();
+    title === 'LOGIN' && setModal.setSignin();
+  };
+  const onChangeOtherModal = () => {
+    resetInput();
     setModal.setSignup();
     setModal.setSignin();
-  }
+  };
 
-  const onChangeInput=(e:React.ChangeEvent<HTMLInputElement>)=>{
-    const {value,name}=e.currentTarget;
-    setInput({type:name as authInfoList,value:value});
-  }
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.currentTarget;
+    setInput({ type: name as authInfoList, value: value });
+  };
 
-  return(
-    <Background>
-      <ModalContainer>
-        <div style={{marginBottom:"20px"}}>{title}</div>
+  return (
+    <Background onClick={onChagneClose}>
+      <ModalContainer onClick={(e)=>{e.stopPropagation()}}>
+        <div style={{ marginBottom: '20px', color: 'black' }}>{title}</div>
         <CloseIcon onClick={onChagneClose}></CloseIcon>
-        {inputs.map((props,i)=>(
+        {inputs.map((props, i) => (
           <InfoInput key={i}>
-          <p>{props.infoName}</p>
-          <input name={props.type} type={props.inputType} onChange={onChangeInput}></input>
-        </InfoInput>
+            <p>{props.infoName}</p>
+            <input
+              name={props.type}
+              type={props.inputType}
+              onChange={onChangeInput}
+              value={loginInfo[props.type]}
+            ></input>
+          </InfoInput>
         ))}
         <Discription onClick={onChangeOtherModal}>{discription}</Discription>
-        <SubmitButton onClick={onLogin}>{title}</SubmitButton>
+        <SubmitButton onClick={onSubmitClick}>{title}</SubmitButton>
       </ModalContainer>
     </Background>
   );
-} 
+};
 
 export default AuthModals;
