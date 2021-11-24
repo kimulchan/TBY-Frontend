152428d836b6c201577992d90b17977da40948b7
kimulchan <leerabi577@gmail.com>
2021-11-24 21:43:48 +0900

    ⚡ :: 여러가지 constance 추가

--- src/constance.ts
+++ src/constance.ts
@@ -1,41 +1,51 @@
 import { authModalType, categoryName, categoryType } from "./types"
 
 
-export const category:categoryName[]=["FrontEnd","BackEnd","Android","iOS",]
+export const category:categoryName[]=["FrontEnd","BackEnd","iOS","Android"]
 
 export const subject:categoryType = {
   FrontEnd:[{
     subjectName:"JavaScript",
-    subjectId:0
+    subjectId:1
   },{
     subjectName:"TypeScript",
-    subjectId:1
+    subjectId:2
   }],
   BackEnd:[{
-    subjectId:0,
+    subjectId:5,
     subjectName:"Java"
   },{
-    subjectId:1,
+    subjectId:6,
     subjectName:"Go"
   },{
-    subjectId:2,
-    subjectName:"JavaScript"
+    subjectId:4,
+    subjectName:"TypeScript"
   },{
     subjectId:3,
+    subjectName:"JavaScript"
+   }
+   ,{
+    subjectId:7,
     subjectName:"Python"
-  }],
+  }
+],
   Android:[{
     subjectName:"Java",
-    subjectId:0
+    subjectId:9
   }],
   iOS:[{
-    subjectId:0,
+    subjectId:8,
     subjectName:"Swift"
   }]
 }
 //api uri
 export const uri={
-  signup:""
+  signup:"/user/signup",
+  signin:"/user/login",
+  adminLogin:"/admin/login",
+  adminAddTitle:"/admin/add_title",
+  adminAddVideo:"/admin/add_video",
+  title:"/title"
 }
 
 //auth modal constance
@@ -51,13 +61,15 @@ export const authModal:authModalType = {
       infoName:"PASSWORD",
       type:"password",
       inputType:"password"
-      
     }]
   },
   signup:{
     title:"JOIN",
     discription:"회원이신가요?",
     inputs:[{
+      infoName:"NAME",
+      type:"name"
+    },{
       infoName:"ID",
       type:"id"
     },{
@@ -67,6 +79,18 @@ export const authModal:authModalType = {
     },{
       infoName:"PASSWORD CHACK",
       type:"passwordCheck",
+      inputType:"password"
+    }]
+  },
+  adminLogin:{
+    title:"ADMIN LOGIN",
+    discription:"",
+    inputs:[{
+      infoName:"ID",
+      type:"id"
+    },{
+      infoName:"PASSWORD",
+      type:"password",
       inputType:"password"
     }]
   }
