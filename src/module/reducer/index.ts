f658c9d4e59b22651d85725fe40c3389e60abe8f
kimulchan <leerabi577@gmail.com>
2021-11-24 21:52:47 +0900

    ⚡ :: reducer 설정

--- src/module/reducer/index.ts
+++ src/module/reducer/index.ts
@@ -1,9 +1,19 @@
 import { combineReducers } from "redux";
 import modalReducer from "./modal";
+import authReducer from "./auth";
+import titleReducer from "./title";
+import videoReducer from "./video";
+import addTitleReducer from "./addTitle";
+import  addVideoReducer  from "./addVideo";
 
 
 const rootReducer = combineReducers({
-  modal:modalReducer
+  modal:modalReducer,
+  auth:authReducer,
+  title:titleReducer,
+  video:videoReducer,
+  addTitle:addTitleReducer,
+  addVideo:addVideoReducer
 })
 
 export type reducerType =ReturnType<typeof rootReducer>
