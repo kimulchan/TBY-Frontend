6165a3e74c3af83d6a04da2ab20ba8069264f2a8
kimulchan <leerabi577@gmail.com>
2021-11-24 21:46:28 +0900

    ⚡ :: auth action 추가

--- src/module/action/auth.ts
+++ src/module/action/auth.ts
@@ -1,14 +1,28 @@
-import {ActionType, createAction} from 'typesafe-actions';
-import { authInputActionType } from '../../types';
+import {ActionType, createAction, createAsyncAction} from 'typesafe-actions';
+import { authInputActionType, onJoinProps, onLoginProps } from '../../types';
 
-export const INPUT = "signup/INPUT";
+export const INPUT = "auth/INPUT";
+export const RESET_INPUT = "auth/RESET_INPUT";
 
-export const ON_LOGIN = "signup/ON_LOGIN";
-export const ON_LOGIN_SUCCESS = "signup/ON_LOGIN_SUCCESS";
-export const ON_LOGIN_FAILURE = "signup/ON_LOGIN_FAILURE";
+export const ON_LOGIN = "auth/ON_LOGIN";
+export const ON_LOGIN_SUCCESS = "auth/ON_LOGIN_SUCCESS";
+export const ON_LOGIN_FAILURE = "auth/ON_LOGIN_FAILURE";
+
+export const ON_JOIN = "auth/ON_JOIN";
+export const ON_JOIN_SUCCESS= "auth/ON_JOIN_SUCCESS";
+export const ON_JOIN_FAILURE = "auth/ON_JOIN_FAILURE";
+
+export const ON_ADMIN_LOGIN = "auth/ON_ADMIN_LOGIN";
+export const ON_ADMIN_LOGIN_SUCCESS= "auth/ON_ADMIN_LOGIN_SUCCESS";
+export const ON_ADMIN_LOGIN_FAILURE = "auth/ON_ADMIN_LOGIN_FAILURE";
 
 export const setInput = createAction(INPUT)<authInputActionType>();
+export const resetInput = createAction(RESET_INPUT)();
 
-const actions = {setInput}
+export const onLogin =createAsyncAction(ON_LOGIN,ON_LOGIN_SUCCESS,ON_LOGIN_FAILURE)<onLoginProps,undefined,undefined>();
+export const onJoin = createAsyncAction(ON_JOIN,ON_JOIN_SUCCESS,ON_LOGIN_FAILURE)<onJoinProps,undefined,undefined>();
+export const onAdminLogin= createAsyncAction(ON_ADMIN_LOGIN,ON_ADMIN_LOGIN_SUCCESS,ON_ADMIN_LOGIN_FAILURE)<onLoginProps,undefined,undefined>();
+
+const actions = {setInput,resetInput,onLogin,onJoin,onAdminLogin}
 
 export type authActions=ActionType<typeof actions>;
