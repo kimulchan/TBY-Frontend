94abdee9d716b41c9761a2539569fc41eab2bd8a
kimulchan <leerabi577@gmail.com>
2021-11-24 22:05:34 +0900

    ⚡ :: auth api 추가

--- src/utils/api/auth.ts
+++ src/utils/api/auth.ts
@@ -1,14 +1,27 @@
 import { AxiosResponse } from "axios";
 import { uri } from "../../constance";
-import { loginInfo, onJoinProps, onLoginProps } from "../../types";
+import { onJoinProps, onLoginProps } from "../../types";
 import { getRequest } from "./default";
-
+const ACCESSTOKEN_TIME = 60*60*24;
 
 export const onLoginApi = async (loginRequest:onLoginProps)=>{
   try{
     const request = getRequest();
-    const response:AxiosResponse<{accessToken:string}> = await request.post(uri.signin,{email:loginRequest.id,password:loginRequest.password});
-    localStorage.setItem("accessToken",response.data.accessToken);
+    const response:AxiosResponse<{access_token:string}> = await request.post(uri.signin,loginRequest);
+    console.log(response);
+    localStorage.setItem("accessToken",response.data.access_token);
+    setTimeout(()=>{localStorage.removeItem("accessToken")},ACCESSTOKEN_TIME);
+  }
+  catch(error){
+    throw error;
+  }
+}
+export const onAdminLoginApi = async(loginRequest:onLoginProps)=>{
+  try{
+    const request = getRequest();
+    const response:AxiosResponse<{access_token:string}> = await request.post(uri.adminLogin,loginRequest);
+    localStorage.setItem("accessToken",response.data.access_token);
+    localStorage.setItem("isAdmin","true");
   }
   catch(error){
     throw error;
@@ -18,7 +31,7 @@ export const onLoginApi = async (loginRequest:onLoginProps)=>{
 export const onJoinApi = async (joinRequest:onJoinProps)=>{
   try{
     const request = getRequest();
-    await request.post(uri.signup,{email:joinRequest.id,password:joinRequest.password,name:joinRequest.name})
+    await request.post(uri.signup,joinRequest);
   }
   catch(error){
     throw error;
