a540ab943973cb430f4f04fa13e64c8f44b8adb4
kimulchan <leerabi577@gmail.com>
2021-11-24 22:04:16 +0900

    ⚡ :: MainRouter 추가

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -3,20 +3,28 @@ import Footer from "../components/footer";
 import Header from "../components/header";
 import MainPage from "../components/main";
 import Modal from "../components/modal";
-import SignUpModal from "../components/modal/SignUp";
 import NotFound from "../components/notFound";
-import VideoList from "../components/videoBox";
+import AdminAddTitleContainer from "../container/Admin/AdminAddTitle";
+import AdminAddVideoContainer from "../container/Admin/AdminAddVideo";
+import AdminLoginContainer from "../container/Admin/AdminLogin";
+import AdminMainContainer from "../container/Admin/AdminMainContainer";
+import TitleListContainer from "../container/TitleList";
+import VideoListContainer from "../container/VideoList";
 
 const MainRouter = ()=>{
   return (
     <>
-    
     <Header/>
     <Modal></Modal>
     <Routes>
       <Route path="/" element={<MainPage></MainPage>}/>
-      <Route path="/lecture/:category" element={<VideoList></VideoList>}/>
-      <Route path="/lecture/:category/:subject"element={<VideoList></VideoList>}/>
+      <Route path="/admin/login" element={<AdminLoginContainer></AdminLoginContainer>}></Route>
+      <Route path="/admin" element={<AdminMainContainer></AdminMainContainer>}/>
+      <Route path="/admin/addVideo" element={<AdminAddVideoContainer/>}/> 
+      <Route path="/admin/addTitle" element={<AdminAddTitleContainer/>}/> 
+      <Route path="/lecture/:category" element={<TitleListContainer></TitleListContainer>}/>
+      <Route path="/lecture/:category/:subject"element={<TitleListContainer></TitleListContainer>}/>
+      <Route path="/video/:id" element={<VideoListContainer></VideoListContainer>}></Route>
       <Route path="*" element={<NotFound></NotFound>}></Route>
     </Routes>
     <Footer/>
